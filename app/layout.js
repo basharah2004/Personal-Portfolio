1086ffcf81faabd366f0ecc039bcb4d3374f1aff
basharah2004 <basharahissa@gmail.com>
2023-10-18 15:33:28 -0700

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: "Basharah's Portfolio",
+  description: 'asdfuhaosudhf',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body >{children}</body>
+    </html>
+  )
+}
